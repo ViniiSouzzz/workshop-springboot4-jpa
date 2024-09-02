3c72d7d983fc86ad48be107e237db1ba23d2c01c
ViniiSouzzz <viniciusedesouza2301@gmail.com>
2024-09-02 11:12:01 -0300

    project created

---
+++ src/main/java/com/projetoTesteVini/projetoT/ProjetoTApplication.java
@@ -0,0 +1,13 @@
+package com.projetoTesteVini.projetoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoTesteVini/projetoT/ProjetoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoTesteVini.projetoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
